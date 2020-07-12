880a77189bd504cbca97e89af7dd875edcfbe008
Artyma93 <artyma93@gmail.com>
2020-07-12 22:17:30 +0300

    Add Contollers and Views in Calabonga.Auth.Orders.Api 7:46-8:35

---
+++ Calabonga.Auth/Calabonga.Auth.Orders.Api/Controllers/SiteController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Calabonga.Auth.Orders.Api.Controllers
+{
+    //[Route("[controller]")]
+    //[ApiController]
+    public class SiteController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
