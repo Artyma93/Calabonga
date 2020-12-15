d8cd27b10e6205123e893f3dfed255859eef8580
Artyma93 <artyma93@gmail.com>
2020-12-15 08:17:45 +0300

    Change Calabonga.Auth.IdentityServer 00:00:00-00:01:37
    Video.Name = Аутентификация: LogOut и IdentityServer (часть 6)
    Video.Url = https://www.youtube.com/watch?v=xleP6mQfgXY
    Github.Calabonga.Commit.Name = Video #6 completed (log out)
    Github.Calabonga.Commit.Hash = 4e5dbb340fcbf7a69c22c323db35cbebd2e2319f

--- Calabonga.Auth/Calabonga.Auth.IdentityServer/Controllers/AuthController.cs
+++ Calabonga.Auth/Calabonga.Auth.IdentityServer/Controllers/AuthController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Calabonga.Auth.IdentityServer.ViewModels;
+using IdentityServer4.Services;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,21 +12,42 @@ namespace Calabonga.Auth.IdentityServer.Controllers
     [Route("[controller]")]
     public class AuthController : Controller
     {
+        private readonly IIdentityServerInteractionService _interactionService;
         private readonly SignInManager<IdentityUser> _signInManager;
         private readonly UserManager<IdentityUser> _userManager;
 
         public AuthController(
+            IIdentityServerInteractionService interactionService,
             SignInManager<IdentityUser> signInManager,
             UserManager<IdentityUser> userManager)
         {
+            _interactionService = interactionService;
             _signInManager = signInManager;
             _userManager = userManager;
         }
 
         [Route("[action]")]
+        public async Task<IActionResult> Logout(string logoutId)
+        {
+            await _signInManager.SignOutAsync();
+            var logoutResult = await _interactionService.GetLogoutContextAsync(logoutId);
+            if (string.IsNullOrEmpty(logoutResult.PostLogoutRedirectUri))
+            {
+                return RedirectToAction("Index", "Site");
+            }
+
+            return Redirect(logoutResult.PostLogoutRedirectUri);
+        }
+
+        [Route("[action]")]
         public IActionResult Login(string returnUrl)
         {
-            return View();
+            return View(new LoginViewModel
+            {
+                Password = "123qwe",
+                ReturnUrl = returnUrl,
+                UserName = "User"
+            });
         }
 
         [HttpPost]
