4b1ce2d401f6b46658c5c42195274add2b3473ed
Artyma93 <artyma93@gmail.com>
2020-12-18 18:32:42 +0300

    Change Calabonga.Auth.JavaScript 00:24:06-00:31:37
    Video.Name = Аутентификация: JavaScript, SPA и IdentityServer4 (часть 8)
    Video.Url = https://www.youtube.com/watch?v=VQRvlBbOgko&t=5s
    Github.Calabonga.Commit.Name = JavaScript with IdentityServer4 #1
    Github.Calabonga.Commit.Hash = e74a2e6f7cd9578a4d35b87b02be9256125edced

---
+++ Calabonga.Auth/Calabonga.Auth.JavaScript/wwwroot/js/callback.js
@@ -0,0 +1,15 @@
+﻿const manager = new Oidc.UserManager({
+    loadUserInfo: true,
+    response_mode: "query"
+});
+
+manager.signinRedirectCallback()
+    .then(function (user) {
+        console.log(user);
+        window.location.href = "https://localhost:9001";
+    })
+    .catch(function (error) {
+        console.log(error);
+    });
+
+
